14bcffdfc339996a0ba2cb6d28257c99c5065c47
dilpreet2012 <dilkill2000@gmail.com>
2024-09-25 15:19:08 +0530

    file upload handle

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ const app=express()
 app.use(cors({
     origin:process.env.CORS_ORIGIN
 }))
+app.get('/',(req,res)=>{
+res.send('home page')
+})
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-export {app}
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import connectdB from "./db/index.js";
+import app from './app.js'
 dotenv.config({
     path:'./env'
 })
