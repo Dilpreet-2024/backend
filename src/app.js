8c73e35b437e6af8cf86766b05569b9dd20078ec
dilpreet2012 <dilkill2000@gmail.com>
2024-09-26 11:58:10 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -5,11 +5,16 @@ const app=express()
 app.use(cors({
     origin:process.env.CORS_ORIGIN
 }))
-app.get('/',(req,res)=>{
-res.send('home page')
-})
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.router.js'
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+
 export default app;
